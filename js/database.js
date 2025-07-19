d2f928d96b5347e8cbca59b1d7c8a54321641cba
info-prt <info.peoples.reform@gmail.com>
2025-07-19 19:04:03 +0700

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,39 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const tableBody = document.querySelector('.dataset-table tbody');
+  const tableBody = document.querySelector('.dataset-table tbody');
 
-    fetch('https://script.google.com/macros/s/AKfycbzDbQ7Yki_UEIMswORobYK7bSdzKiQXh_8Kd_X-yYHcXSu-ThVQhomBX_YRk5cs_aw/exec')
-        .then(response => response.json())
-        .then(data => {
-            tableBody.innerHTML = ''; // ล้างข้อมูลเดิม
+  fetch('https://script.google.com/macros/s/AKfycbzDbQ7Yki_UEIMswORobYK7bSdzKiQXh_8Kd_X-yYHcXSu-ThVQhomBX_YRk5cs_aw/exec')
+    .then(response => response.json())
+    .then(data => {
+      tableBody.innerHTML = ''; // ล้างข้อมูลเดิม
 
-            data.forEach(item => {
-                const row = document.createElement('tr');
+      data.forEach(item => {
+        const row = document.createElement('tr');
 
-                const nameCell = `<td>${item.name}</td>`;
-                const divisionCell = `<td>${item.division}</td>`;
-                const typeCell = `<td>${item.type}</td>`;
-                const dateCell = `<td>${item.date}</td>`;
+        const nameCell = `<td>${item.name}</td>`;
+        const divisionCell = `<td>${item.division}</td>`;
+        const typeCell = `<td>${item.type}</td>`;
+        const dateCell = `<td>${item.date}</td>`;
 
-                const fileId = extractDriveId(item.file);
-                const downloadCell = `
-              <td>
-                <a class="download-btn" href="https://drive.google.com/uc?export=download&id=${fileId}" target="_blank">
-                  <i class="fas fa-download"></i> ดาวน์โหลด
-                </a>
-              </td>`;
+        const downloadCell = item.url
+          ? `<td>
+              <a class="download-btn" href="${item.url}" target="_blank">
+                <i class="fas fa-download"></i> ดาวน์โหลด
+              </a>
+            </td>`
+          : `<td><span style="color: gray;">ไม่มีไฟล์</span></td>`;
 
-                row.innerHTML = nameCell + divisionCell + typeCell + dateCell + downloadCell;
-                tableBody.appendChild(row);
-            });
-        })
-        .catch(error => {
-            tableBody.innerHTML = `<tr><td colspan="5">เกิดข้อผิดพลาดในการโหลดข้อมูล</td></tr>`;
-            console.error('เกิดข้อผิดพลาด:', error);
-        });
-
-    function extractDriveId(url) {
-        const regex = /[-\w]{25,}/;
-        const match = url.match(regex);
-        return match ? match[0] : '';
-    }
+        row.innerHTML = nameCell + divisionCell + typeCell + dateCell + downloadCell;
+        tableBody.appendChild(row);
+      });
+    })
+    .catch(error => {
+      console.error('เกิดข้อผิดพลาด:', error);
+      tableBody.innerHTML = `<tr><td colspan="5">ไม่สามารถโหลดข้อมูลได้</td></tr>`;
+    });
 });
